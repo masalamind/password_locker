fea30676b1d31149bd932ee86340781a3d55b327
masalamind <themasalacollective@gmail.com>
2022-04-25 16:02:02 +0300

    Logic to add a user

--- main.py
+++ main.py
@@ -16,6 +16,51 @@ print("""
 print("""-----------------------------------------------------------------------------------------------------------""")
 print("\n")
 
+# DATA STRUCTURES
+mwandani_app_store = []
+mwandani_user_store = []
+
+
+class Application:
+
+    def __init__(self, name, password):
+        self.app = {}
+        self.name = name
+        self.password = password
+        app.app_name = self.name
+        app.app_password = self.password
+
+        mwandani_app_store.append(app)
+
+    def register_app(self):
+        user_app_name = (input("Enter application's name: ")).lower()
+        user_app_password = input("Enter application's password: ")
+        self.app_name = Application(user_app_name, user_app_password)
+
+
+class Mwandani_user:
+
+    def __init__(self, user_first_name, user_middle_name, user_username, user_password):
+        self.user = {
+            'first_name': user_first_name ,
+            'middle_name': user_middle_name,
+            'user_name': user_username,
+            'password': user_password
+        }
+
+    def register_user(self):
+        # Add new user object to list of Mwandani users
+        mwandani_user_store.append(self.user)
+
+    def login_user(self, login_name, login_password):
+        if login_name == self.username and login_password == self.password:
+            return True
+        else:
+            return False
+
+# BUSINESS LOGIC
+
+
 running = True
 
 while running:
@@ -23,24 +68,32 @@ while running:
     program_active = input("Press Y to proceed or Type 'quit' to exit: ")
 
     if program_active.lower() == 'quit' or program_active == 'q':
-        print("\nWe're sad to see you go :-(. Catch you later though.\n")
+        print("\nWe're sad to see you go :-( Catch you later though.\n")
         running = False
         break
 
     if program_active.lower() == 'y' or program_active.lower() == 'yes':
         print("\nSelect an option number to proceed: ")
         print("""
-                1. Signup on Mwandani
-                2. Sign into your Mwandani Account
-                3. Manage your Mwandani Account
-                4. Forgotten Password?
+        1. Signup on Mwandani
+        2. Sign into your Mwandani Account
+        3. Manage your Mwandani Account
+        4. Forgotten Password?
 
-            """)
+        """)
 
         navigation_option = int(input(": "))
 
         if navigation_option == 1:
-            print("signup the user")
+            print("Hello, Kindly enter your")
+            first_name = input("Enter Your First Name: ")
+            middle_name = input("Enter Your Middle Name: ")
+            username = input("Enter a Mwandani Username: ")
+            password = input("Enter a new Mwandani Password: ")
+
+            new_user = Mwandani_user(first_name, middle_name, username, password)
+            new_user.register_user()
+
         if navigation_option == 2:
             print("sign in the user")
         if navigation_option == 3:
@@ -51,10 +104,45 @@ while running:
     else:
             print("!Kindly enter either 'quit' or 'y'")
 
+for user in mwandani_user_store:
+    print(user)
+# for app in mwandani_app_store:
+#     print(app)
+
 print(":-) Thank you for choosing Mwandani. We got your back.")
 
 
-# DATA STRUCTURES
+
+
+
+
+    # def view_accounts(self):
+    #     print("""
+    #             1. Enter application name
+    #             2. View all applications credentials
+    #
+    #             """)
+    #     view_accounts_mode = int(input(": "))
+    #
+    #     if view_accounts_mode == 1:
+    #         user_app_name = (input("Enter application name: ")).lower()
+    #         user_pwd = input("Enter Mwandani Password: ")
+    #         # if user_pwd == mwandani_user_db.self.username == user_pwd: # db stored as key value pairs usrnm: pwed
+    #             # mwandani_app_db.query user
+    #
+    #
+    #     elif view_accounts_mode == 2:
+    #         user_pwd = input("Enter Mwandani Password: ")
+    #         # Iterate all items in the mwandani_user_db
+    #
+    #     else:
+    #         print("!Kindly enter either 1 or 2")
+    #
+
+
+
+
+
 
 
 
