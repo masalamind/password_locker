de13ee4a0baad17d9b154f28315440cd116bae84
masalamind <themasalacollective@gmail.com>
2022-04-26 11:35:48 +0300

    Add docstrings to methods and classes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """ Password Locker Application. """
+# Dependencies for generating random passwords
 import string
 import random
-# NAVIGATION UI
+
+# APPLICATION HEADER
 
 print("\n")
 print("""-----------------------------------------------------------------------------------------------------------""")
@@ -17,36 +19,41 @@ print("""
 print("""-----------------------------------------------------------------------------------------------------------""")
 print("\n")
 
-
-
-
 # DATA STRUCTURES FOR STORING OBJECTS
+
 mwandani_app_store = []
 mwandani_user_store = []
 
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
-def new_random_password():
+
+def new_random_password(apps_name):
+    """Returns a new randomly generated password. Takes password length as input."""
+
     pwd_length = int(input("Please enter the password length: "))
-
     random.shuffle(characters)
-
     password = []
+
     for i in range(pwd_length):
         password.append(random.choice(characters))
 
     random.shuffle(password)
     new_password = "".join(password)
+    print(f"{apps_name}'s new password is {new_password}")
+
     return new_password
 
-def createPassword(option):
+
+def createPassword(app_name, option):
+    """Decides whether to outsource password creation to password generator function."""
+
     if option == 2:
-        return new_random_password()
+        return new_random_password(app_name)
     else:
         return
 
 
-
 class Application:
+    """Creates application objects with app name and password to later be stored in a list."""
 
     def __init__(self, name, password):
         self.app = {
@@ -59,10 +66,10 @@ class Application:
 
 
 class Mwandani_user:
+    """Creates user objects. Main program takes in credentials then instantiates a Mwandani_user class."""
 
     def __init__(self, user_first_name, user_middle_name, user_username, user_password):
         self.user = {
-
             'first_name': user_first_name ,
             'middle_name': user_middle_name,
             'user_name': user_username,
@@ -79,14 +86,11 @@ class Mwandani_user:
         else:
             return False
 
-
-
 # BUSINESS LOGIC
 
 running = True
 
 while running:
-
     program_active = input("PRESS 'y' TO PROCEED OR TYPE 'quit' TO EXIT APPLICATION: ")
 
     if program_active.lower() == 'quit' or program_active == 'q':
@@ -106,16 +110,18 @@ while running:
 
         navigation_option = int(input(": "))
 
-        # these options are mutually exclusive so if a user signups and creates an object in one option - the logic
-        # works if the user also creates a new object under another option
+        # Above options are mutually exclusive so if a user signups and creates an object in one option
+        # the logic will continue to work only if he does os again in new option
+        # improve: Make more DRY
+
         if navigation_option == 1:
-            print("To signup on Mwandani, kindly enter your")
+            print("To signup on Mwandani, kindly enter ...")
             first_name = input("Your First Name: ")
             middle_name = input("Your Middle Name: ")
             username = input("Please enter a unique Mwandani Username: ")
             password = input("Please enter a new Mwandani Password: ")
             print("\n--------------------------------------------------")
-            print("|  \o/ Success you now have a Mwandani account    |")
+            print("\t\t\o/ Success you now have a Mwandani account\t\t")
             print("--------------------------------------------------\n")
 
             current_user = Mwandani_user(first_name, middle_name, username, password)
@@ -169,22 +175,18 @@ while running:
                 while True:
                     print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
                     user_app_name = (input("Please enter application's name: ")).lower()
-                    print("""Password Options: \n 1. Enter Own Password \n 2. Suggest a password for me
-                          
-                    """)
-                    user_password_option = int(input("1 or 2: "))
+                    print(f"{user_app_name}'s Password Options: \n1. Enter Own Password \n2. Suggest a password for me")
+                    user_password_option = int(input("\n1 or 2: "))
 
                     if user_password_option == 1:
                         user_app_password = input("Enter application's password: ")
                     if user_password_option == 2:
-                        user_app_password = createPassword(user_password_option)
-
-                    #user_suggested_password = input("Suggest a password for me: ")
+                        user_app_password = createPassword(user_app_name, user_password_option)
 
                     new_app = Application(user_app_name, user_app_password)
                     new_app.register_app()
 
-                    registration = input("Register another app? (y/n): ")
+                    registration = input("\nRegister another app? (y/n): ")
                     if registration.lower() == 'y':
                         continue
                     if registration.lower() == 'n':
@@ -192,50 +194,48 @@ while running:
 
             if sign_in_step == 2:
                 user_app_name = (input("Type the app's name and press ENTER to proceed: ")).lower()
-                # print(mwandani_app_store)
+
                 for app in mwandani_app_store:
                     if app['app_name'] == user_app_name:
                         print("_______________________________________________________________________________________")
-                        print(f"\t\t{app['app_name']} : {app['app_password']} ")
+                        print(f"\t\t\t{app['app_name']} : {app['app_password']} ")
                         print("_______________________________________________________________________________________")
 
             if sign_in_step == 3:
                 for app in mwandani_app_store:
                     print("___________________________________________________________________________________________")
-                    print(f"\t\tUsername: {app['app_name']}  Password: {app['app_password']}")
+                    print(f"\t\t\tUsername: {app['app_name']}  Password: {app['app_password']}")
                     print("___________________________________________________________________________________________")
 
             if sign_in_step == 4:
                 user_app_name = input("Type the app's name and press ENTER to delete [X]: ")
-                # the range is set only once with initial list length thus we need to break less you get an out of index error
+
+                # Range set once with initial length thus need to break after deletion else error 4 nxt iteration.
                 for i in range(len(mwandani_app_store)):
                     if mwandani_app_store[i]['app_name'] == user_app_name:
                         del mwandani_app_store[i]
-                        print(f"{user_app_name} has been deleted successfuly.\o/")
-                        break;  # the next iteration will contain the initial number of lists not the no of lists - 1
+                        print(f"\o/{user_app_name} has been deleted successfuly.")
+                        break  # the next iteration will contain the initial number of lists not the length of lists - 1
 
             else:
                 pass
 
         if navigation_option == 3:
             print("manage users account")
+            print("Coming soon: Allows you to update your mwandani username and password")
+
         if navigation_option == 4:
             print("user password recovery")
+            print("Coming soon: Allows you to set password hint, recover forgotten passwords etc.")
 
     else:
         print("!Kindly enter either 'quit' or 'y'")
 
+print("@)- Thank you for choosing Mwandani \n \t\t We got your back.")
 
 
-print(":-) Thank you for choosing Mwandani. \n \t\t We got your back.")
 
 
-# VIEW THE DATABASES
-
-# for user in mwandani_user_store:
-#     print(user)
-# for app in mwandani_app_store:
-#     print(app)
 
 
 
