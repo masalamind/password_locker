b9dfd928bca12a8e6a447a9804d89e9a77e4f55e
masalamind <themasalacollective@gmail.com>
2022-04-26 15:38:22 +0300

    Fixed bugs for application_test.py

--- application_test.py
+++ application_test.py
@@ -1,11 +1,16 @@
 from main import Application
 
+data_store = []
 class create_application_credentials:  
+  
     def __init__(self):
         self.name = 'an_application_name'
-        self.password = 'an_application_password'
-    
-    return Application(self.name, self.password)
+        self.password = 'an_application_password'    
+        return Application(self.name, self.password)
   
 class register_application_in_data_store:
-  def__init__(self):
+
+    def __init__(self):
+      app = Application('the_app','its_password')
+      return data_store.append(app)
+    
