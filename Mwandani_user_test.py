6447a3af5ce338f7ec68f65096ffc9fb7bc494ce
masalamind <themasalacollective@gmail.com>
2022-04-26 15:14:21 +0300

    Add test class for instantiating a user

--- Mwandani_user_test.py
+++ Mwandani_user_test.py
@@ -1,8 +1,21 @@
+import Mwandani_user from main
 
+
+data_store = []
 class create_create_a_user_account:
+  def __init__(self):
+    self.firstname = 'john'
+    self.middlename = 'doe'
+    self.username = 'johndoe'
+    self.password = '@john'
+    
+    return Mwandani_user(self.firstname, self.middlename,self.username,self.password)
   
 class register_a_user_in_the_data_store:
+  def__init__(self):
+    
   
 class login_a_user:
-  
+  def__init__(self):
+    
   
---
+++ application_test.py
@@ -0,0 +1,8 @@
+
+
+class create_application_credentials:
+  def__init__(self):
+    
+  
+class register_application_in_data_store:
+  def__init__(self):
