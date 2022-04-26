3b585cfcfeb51c1ac57f99c79e5b76f7180e1b45
masalamind <themasalacollective@gmail.com>
2022-04-26 15:18:33 +0300

    Add a test suite for instantiating an application

--- application_test.py
+++ application_test.py
@@ -1,8 +1,11 @@
-
+import Application from main
 
 class create_application_credentials:
   def__init__(self):
+    self.name = 'an_application_name'
+    self.password = 'an_application_password'
     
+    return Application(self.name, self.password)
   
 class register_application_in_data_store:
   def__init__(self):
